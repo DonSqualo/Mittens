3f3eeaaa89b467f9c7779ad74536a6f3405f6ba3
Heye Gross <me@heye.earth>
2026-02-02 21:56:18 +0000

    LymphSim: Adjust camera position for better view

--- projects/lymphsim/main.lua
+++ projects/lymphsim/main.lua
@@ -41,11 +41,12 @@ local lymphsim = assembly("LymphSim", {
   version = "0.1.0",
 })
 
--- Set up the view
+-- Set up the view - camera closer to see details
+-- Bath is about 2m x 0.6m, centered around (1.02, 0.32, 0.37)
 view({
   camera = {
-    position = {2.5, 1.5, 1.2},
-    target = {Chamber.center_x, Chamber.center_y, Chamber.water_surface_z},
+    position = {3.5, 2.5, 2.0},  -- Closer and higher view
+    target = {1.0, 0.3, 0.2},    -- Bath center
     up = {0, 0, 1},
   },
   background = {0.1, 0.1, 0.15, 1.0},
